f3c18aa5359c5f0f0d48992aaf3d9f8809db7019
adilmer <adil.lmerzougui@gmail.com>
2023-06-25 09:36:07 +0100

    update menu design

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,4 +1,4 @@
-<nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+<nav class="sidebar-nav scroll-sidebar" >
     <ul id="sidebarnav">
       <li class="nav-small-cap">
         <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
@@ -15,62 +15,95 @@
         <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
       </li>
       <li class="sidebar-item">
-        <a class="sidebar-link" href="./ui-buttons.html" aria-expanded="false">
+        <a class="sidebar-link" href="#" data-bs-toggle="dropdown" >
           <span>
             <i class="ti ti-users"></i>
           </span>
           <span class="hide-menu">الموظفين</span>
         </a>
+        <ul class="dropdown-menu text-end">
+          <li> <a class="dropdown-item" href="agent.html"> لائحة الموظفين </a></li>
+          <li> <a class="dropdown-item" href="ilhaq.html"> الإلحاق</a> </li>
+          <li> <a class="dropdown-item" href="idmaj.html"> الإدماج</a> </li>
+          <li> <a class="dropdown-item" href="rahnichara.html"> رهن إشارة</a> </li>
+          <li> <a class="dropdown-item" href="retraite.html"> التقاعد</a> </li>
+          <li> <a class="dropdown-item" href="#"> الترقية</a> </li>
+          <li> <a class="dropdown-item" href="#"> الحضور</a> </li>
+          <li> <a class="dropdown-item" href="#"> الكفاءة المهنية</a> </li>
+        </ul>
       </li>
       <li class="sidebar-item">
-        <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
-          <span>
-            <i class="ti ti-alert-circle"></i>
-          </span>
-          <span class="hide-menu">الشواهد الادارية</span>
-        </a>
-      </li>
-      <li class="sidebar-item">
-        <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
+        <a class="sidebar-link" href="#" data-bs-toggle="dropdown" >
           <span>
             <i class="ti ti-cards"></i>
           </span>
-          <span class="hide-menu">الرخص الإدارية</span>
+          <span class="hide-menu">الشواهد الادارية</span>
         </a>
-      </li>
+        <ul class="dropdown-menu text-end">
+          <li> <a class="dropdown-item" href="createdemande.html"> طلب وتيقة</a></li>
+          <li> <a class="dropdown-item" href="ordervirement.html"> أمر بتحويل لا رجعة فيه</a> </li>
+        </ul>
+        </li>
       <li class="sidebar-item">
-        <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
+        <a class="sidebar-link" href="# " data-bs-toggle="dropdown">
           <span>
             <i class="ti ti-file-description"></i>
+          </span>
+          <span class="hide-menu">الرخص الإدارية</span>
+        </a>
+        <ul class="dropdown-menu text-end">
+          <li> <a class="dropdown-item" href="createconge.html"> طلب رخصة</a></li>
+          <li> <a class="dropdown-item" href="conge.html"> ارشيف الرخص</a> </li>
+        </ul>
+      </li>
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="archive.html" aria-expanded="false">
+          <span>
+            <i class="ti ti-archive"></i>
           </span>
           <span class="hide-menu">الأرشيف</span>
         </a>
       </li>
-      <li class="sidebar-item">
-        <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
+      <li class="sidebar-item" id="myDropdown">
+        <a class="sidebar-link" href="#" data-bs-toggle="dropdown">
           <span>
-            <i class="ti ti-typography"></i>
+            <i class="ti ti-book-2"></i>
           </span>
           <span class="hide-menu">التكوينات</span>
         </a>
+        <ul class="dropdown-menu text-end">
+          <li> <a class="dropdown-item" href="formation.html"> لائحة التكوينات </a></li>
+          <li> <a class="dropdown-item" href="creatformation.html"> تكوين جديد </a> </li>
+        </ul>
       </li>
-      <li class="sidebar-item">
-        <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
+      <li class="sidebar-item"  id="myDropdown">
+        <a class="sidebar-link" href="#" data-bs-toggle="dropdown">
           <span>
-            <i class="ti ti-login"></i>
+            <i class="ti ti-user"></i>
           </span>
-          <span class="hide-menu">المتدربين</span>
+          <span class="hide-menu"> المتدربين</span>
         </a>
+        <ul class="dropdown-menu text-end">
+          <li> <a class="dropdown-item" href="stagiaire.html"> لائحة المتدربين </a></li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> متدرب جديد </a> </li>
+        </ul>
       </li>
-      <li class="sidebar-item">
-        <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
+      <li class="sidebar-item"  id="myDropdown">
+        <a class="sidebar-link" href="#" data-bs-toggle="dropdown">
           <span>
-            <i class="ti ti-user-plus"></i>
+            <i class="ti ti-file-description"></i>
           </span>
-          <span class="hide-menu">القرارات</span>
+          <span class="hide-menu">مكتبة الوثائق</span>
         </a>
+        <ul class="dropdown-menu text-end">
+          <li> <a class="dropdown-item" href="stagiaire.html">قرار الإلحاق</a></li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> طلب الإلحاق</a> </li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> قرار الإدماج </a> </li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> طلب الإدماج </a> </li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> طلب الوضع رهن إشارة  </a> </li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> قرار الوضع رهن إشارة  </a> </li>
+          <li> <a class="dropdown-item" href="createstagiaire.html"> قرار الحذف </a> </li>
+        </ul>
       </li>
-
     </ul>
-
   </nav>
