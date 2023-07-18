0b9ce37f970a90583b4fef3ac14767e67107a00b
walidelmerzougui <ahrar96tv@gmail.com>
2023-07-18 21:10:34 +0200

    document and design

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -21,16 +21,12 @@
                 <div class="message-body">
                   <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
                     <i class="ti ti-user fs-6"></i>
-                    <p class="mb-0 fs-3">My Profile</p>
+                    <p class="mb-0 fs-3">حسابي</p>
                   </a>
                   <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                    <i class="ti ti-mail fs-6"></i>
-                    <p class="mb-0 fs-3">My Account</p>
-                  </a>
-                  <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                    <i class="ti ti-list-check fs-6"></i>
-                    <p class="mb-0 fs-3">My Task</p>
-                  </a>
+                    <i class="ti ti-settings fs-6"></i>
+                    <p class="mb-0 fs-3"> الإعدادات</p>
+                  </a> 
                   <a href="./authentication-login.html" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
                 </div>
               </div>
