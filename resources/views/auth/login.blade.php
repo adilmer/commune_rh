2190371ac50faa8753ad338f323aa4fae500b565
walidelmerzougui <ahrar96tv@gmail.com>
2024-01-05 09:42:56 +0100

    NEW UP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,53 @@
 @extends('templates.vide')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<body class="text-center">
+<form class="form-signin" method="POST" action="{{ route('login') }}">
+       @csrf
+      <img class="mb-4" src="{{asset('assets/images/logo.png')}}" alt="" width="80" height="120">
+      <h1 class="h3 mb-3 font-weight-normal">تسجيل الدخول</h1>
+      <label for="inputEmail" class="sr-only">بيانات الدخول</label>
+      <input id="email" type="email" placeholder="البريد الالكتروني" class="form-control text-left @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="اسم المستخدم" autofocus>
+      @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+      @enderror
+      <label for="inputPassword" class="sr-only">الرمز السري</label>
+      <input id="password" type="password" class="form-control text-left @error('password') is-invalid @enderror" placeholder="الرمز السري" name="password" required autocomplete="الرمز السري">
+      @error('password')
+      <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+       </span>
+      @enderror
+      <div class="checkbox mb-3">
+        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+      <label class="form-check-label" for="remember">
+        {{ __('حفظ الدخول') }}
+      </label>
+      </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+      <div class="divlogin">
+        <button type="submit" class="btn btn-lg btn-primary btn-block">
+            {{ __('دخول') }}
+        </button>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+        @if (Route::has('password.request'))
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('نسيت كلمة السر ؟') }}
+            </a>
+        @endif
     </div>
-</div>
+
+
+
+
+      <p class="mt-5 mb-3 text-muted">© 2023 بلدية طانطان</p>
+    </form>
+</body>
+
+
+
 @endsection
--- resources/views/templates/vide.blade.php
+++ resources/views/templates/vide.blade.php
@@ -7,35 +7,26 @@
   <title>الموارد البشرية - جماعة طانطان</title>
   <link rel="shortcut icon" type="image/png" href="{{asset('assets/images/logos/favicon.png')}}" />
   <link rel="stylesheet" href="{{asset('assets/css/styles.min.css')}}" />
-  <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}" />
+  <link rel="stylesheet" href="{{asset('assets/css/styleslogin.css')}}" />
   <link rel="stylesheet" href="{{asset('assets/libs/apexcharts/dist/apexcharts.css')}}" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@500&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
     @yield('style');
 </style>
 </head>
 
 <body>
-  <!--  Body Wrapper -->
-  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-    data-sidebar-position="fixed" data-header-position="fixed">
-    <!-- Sidebar Start -->
-
-    <!--  Main wrapper -->
-    <div class="body-wrapper">
-      <!--  Header Start -->
-
-<!-- Sidebar navigation-->
-
-<!-- End Sidebar navigation -->
- <!--  Header End -->
-    <div class="containe col-11 mx-auto">
 
         @yield('content')
 
-    </div>
+
 
 
   <script src="{{asset('assets/libs/jquery/dist/jquery.min.js')}}"></script>
